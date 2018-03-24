720c0451bf6d8e7b10af160282d437ac0b00e2dd
0x4z <0x4z@openmailbox.org>
2018-03-24 03:39:45 +0100

    Added AboutActivity Added AlbumsActivity Added SongsActivity

--- app/src/main/java/com/example/jerome/musiclibrary/MainActivity.java
+++ app/src/main/java/com/example/jerome/musiclibrary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jerome.musiclibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSongs(View view){
+        Intent i = new Intent(this, SongsActivity.class);
+        startActivity(i);
+    }
+
+    public void onClickAlbums(View view){
+        Intent i = new Intent(this, SongsActivity.class);
+        startActivity(i);
+    }
+
+    public void onClickAbout(View view){
+        Intent i = new Intent(this, SongsActivity.class);
+        startActivity(i);
     }
 }
