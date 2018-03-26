0cd5cb11c9c04e9ffa76828ccccda11a54df2245
braintoilet <jerome.grassnick@gmail.com>
2018-03-26 03:05:14 +0200

    Changed MainActivity layout Added OnClickListener events for all menu items

--- app/src/main/java/com/example/jerome/musiclibrary/MainActivity.java
+++ app/src/main/java/com/example/jerome/musiclibrary/MainActivity.java
@@ -1,30 +1,49 @@
 package com.example.jerome.musiclibrary;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView songsMenu;
+    TextView albumsMenu;
+    TextView aboutMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickSongs(View view){
-        Intent i = new Intent(this, SongsActivity.class);
-        startActivity(i);
-    }
+        songsMenu = (TextView) findViewById(R.id.menu_item_songs);
+        albumsMenu = (TextView) findViewById(R.id.menu_item_albums);
+        aboutMenu = (TextView) findViewById(R.id.menu_item_about);
 
-    public void onClickAlbums(View view){
-        Intent i = new Intent(this, SongsActivity.class);
-        startActivity(i);
-    }
+        songsMenu.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void onClickAbout(View view){
-        Intent i = new Intent(this, SongsActivity.class);
-        startActivity(i);
+        albumsMenu.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AlbumsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        aboutMenu.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
