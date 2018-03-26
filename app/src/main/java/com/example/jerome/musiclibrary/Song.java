2b81148cf57fee7270d1eef4fcbf38ecdea3ec49
braintoilet <jerome.grassnick@gmail.com>
2018-03-26 06:32:39 +0200

    Added Song class Created song_list_item.xml layout (testing)

---
+++ app/src/main/java/com/example/jerome/musiclibrary/Song.java
@@ -0,0 +1,34 @@
+/**
+ * Created by jay on 26.03.18.
+ */
+package com.example.jerome.musiclibrary;
+
+public class Song {
+    private String songName;
+    private String songAlbum;
+    private String songArtist;
+    private int songImageID;
+
+    public Song(String name, String album, String artist, int imageID){
+        songName = name;
+        songAlbum = album;
+        songArtist = artist;
+        songImageID = imageID;
+    }
+
+    public String getSongNameName(){
+        return songName;
+    }
+
+    public String getSongAlbum(){
+        return songAlbum;
+    }
+
+    public String getSongArtist(){
+        return songArtist;
+    }
+
+    public int getSongImageID(){
+        return songImageID;
+    }
+}
--- app/src/main/java/com/example/jerome/musiclibrary/SongsActivity.java
+++ app/src/main/java/com/example/jerome/musiclibrary/SongsActivity.java
@@ -3,11 +3,18 @@ package com.example.jerome.musiclibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class SongsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        ArrayList<Song> songList = new ArrayList<Song>();
+        for(int i = 1; i <= 10; i++){
+            songList.add(new Song("SongName" + i, "Album" + i, "Artist" + i, R.drawable.song_image));
+        }
     }
 }
